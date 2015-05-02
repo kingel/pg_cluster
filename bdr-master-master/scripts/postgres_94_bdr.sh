8d7b8a1fe45975d97af32b441e8784b45f3cfbbe
Franklin Kingma <franklin@fourdigits.nl>
2015-05-02 23:26:49 +0200

    Stop postgres server

--- bdr-master-master/scripts/postgres_94_bdr.sh
+++ bdr-master-master/scripts/postgres_94_bdr.sh
@@ -10,6 +10,7 @@ wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-
 sudo apt-get update
 sudo apt-get install -y git build-essential flex bison libreadline6 libreadline6-dev zlibc zlib1g zlib1g-dev openssl libssl-dev
 sudo apt-get install -y postgresql-9.4 postgresql-client-common postgresql-client-9.4 postgresql-contrib-9.4 postgresql-common
+sudo service postgresql stop
 HOME=/var/lib/postgresql sudo -u postgres bash -c "$(curl -s 'http://git.postgresql.org/gitweb/?p=2ndquadrant_bdr.git;a=blob_plain;f=scripts/bdr_quickstart.sh;hb=bdr-plugin/next')"
 export PATH=/var/lib/postgresql/2ndquadrant_bdr/bdr/bin:$PATH
 
